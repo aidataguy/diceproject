d9db3588047a03957e57d72be5c2db86db36a983
Himanshu patel <himanshu.patel@harbingergroup.com>
2019-09-13 14:45:05 +0530

    State of game added

--- app.js
+++ app.js
@@ -8,66 +8,63 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
-var scores, roundScore, activePlayer;
-
-scores = [0, 0];
-roundScore = 0;
-activePlayer = 0;
-
-// document.querySelector("#current-" + activePlayer).textContent = dice;
-
-// var x = document.querySelector("#score-0").textContent;
-// console.log(x);
-
-// Hide Display :)
-document.querySelector(".dice").style.display = "none";
-
-document.getElementById("score-0").textContent = "0";
-document.getElementById("score-1").textContent = "0";
-document.getElementById("current-0").textContent = "0";
-document.getElementById("current-1").textContent = "0";
+var scores, roundScore, activePlayer, gamePlaying;
+init();
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
-  /**
-   * random number
-   *  */
+  if (gamePlaying) {
+    /**
+     * random number
+     *  */
 
-  dice = Math.floor(Math.random() * 6) + 1;
-  /**
-   * Display results
-   */
-  var diceDOM = document.querySelector(".dice");
-  diceDOM.style.display = "block";
-  /**
-   * display correct dice image
-   */
+    dice = Math.floor(Math.random() * 6) + 1;
+    /**
+     * Display results
+     */
+    var diceDOM = document.querySelector(".dice");
+    diceDOM.style.display = "block";
+    /**
+     * display correct dice image
+     */
 
-  diceDOM.src = "dice-" + dice + ".png";
+    diceDOM.src = "dice-" + dice + ".png";
 
-  /**
-   * update the score if number is NOT a 1
-   */
-  if (dice !== 1) {
-    //   add score
-    roundScore += dice;
-    document.querySelector("#current-" + activePlayer).textContent = roundScore;
-  } else {
-    nextPlayer();
+    /**
+     * update the score if number is NOT a 1
+     */
+    if (dice !== 1) {
+      //   add score
+      roundScore += dice;
+      document.querySelector(
+        "#current-" + activePlayer
+      ).textContent = roundScore;
+    } else {
+      nextPlayer();
+    }
   }
 });
+
 document.querySelector(".btn-hold").addEventListener("click", function() {
-  // add current score to global score and update UI
-  scores[activePlayer] += roundScore;
-  // update UI
-  document.querySelector("#score-" + activePlayer).textContent =
-    scores[activePlayer];
-  // check if player won the game .....
-  if (scores[activePlayer] >= "100") {
-    document.querySelector("#name-" + activePlayer).textContent = "Winner!!";
-    document.querySelector(".dice").style.display = "none";
+  if (gamePlaying) {
+    // add current score to global score and update UI
+    scores[activePlayer] += roundScore;
+    // update UI
+    document.querySelector("#score-" + activePlayer).textContent =
+      scores[activePlayer];
+    // check if player won the game .....
+    if (scores[activePlayer] >= "100") {
+      document.querySelector("#name-" + activePlayer).textContent = "Winner!!";
+      document.querySelector(".dice").style.display = "none";
+      gamePlaying = false;
+    } else {
+      nextPlayer();
+    }
   }
-  nextPlayer();
 });
+
+// New Game Function
+
+document.querySelector(".btn-new").addEventListener("click", init);
 
 function nextPlayer() {
   // next player
@@ -80,4 +77,20 @@ function nextPlayer() {
   document.querySelector(".player-0-panel").classList.toggle("active");
   document.querySelector(".player-1-panel").classList.toggle("active");
   document.querySelector(".dice").style.display = "none";
+}
+
+function init() {
+  scores = [0, 0];
+  roundScore = 0;
+  activePlayer = 0;
+  gamePlaying = true;
+
+  // Hide Display :)
+  document.querySelector(".dice").style.display = "none";
+  document.getElementById("score-0").textContent = "0";
+  document.getElementById("score-1").textContent = "0";
+  document.getElementById("current-0").textContent = "0";
+  document.getElementById("current-1").textContent = "0";
+  document.getElementById("name-0").textContent = "Player 1";
+  document.getElementById("name-1").textContent = "Player 2";
 }
